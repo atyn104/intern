7a606a13e8c0e6049e67348df811f6c34f69d9ed
atyn104 <s22a0014@siswa.umk.edu.my>
2026-03-26 15:41:08 +0800

    Refactor load_data function and update DATA_URL
    Refactor data loading and update variable names.

--- homepage.py
+++ homepage.py
@@ -1,54 +1,18 @@
-import streamlit as st
 import pandas as pd
 import plotly.express as px
+import streamlit as st
+import numpy as np
 
-# URL untuk fail data AI & DS Job Market
-url = "https://raw.githubusercontent.com/atyn104/intern/refs/heads/main/project_data_data.csv"
+# URL Data
+DATA_URL = "https://raw.githubusercontent.com/atyn104/intern/refs/heads/main/project_data_data.csv"
 
-# Set page configuration
-st.set_page_config(
-    page_title="AI & DS Job Market",
-    layout="wide"
-)
-
-st.title("💼 AI & DS Job Market ")
-
-st.markdown("""
-This dataset contains job market information related to Data Science and Artificial Intelligence. It contains information such as job title, experience level, type of work (Remote/Onsite), and required skills such as Python, SQL, etc.
-""")
-
-st.markdown("---")
-
-# Function to load data with caching
 @st.cache_data
-def load_data(data_url):
-    try:
-        df = pd.read_csv(data_url)
-        return df
-    except Exception as e:
-        st.error(f"An error occurred while loading the file from the URL: {e}")
-        return pd.DataFrame()
+def load_data():
+    df = pd.read_csv(DATA_URL)
+    return df
 
-# Load data
-df = load_data(url)
-
-if df.empty:
-    st.stop()
-
-# --- KOMPUTASI DATA ---
-
-# 1. Purata Pengalaman Kerja (Years of Experience)
-avg_experience = data['years_experience'].mean()
-
-# 2. Jumlah pekerjaan & pecahan remote
-total_jobs = len(data)
-remote_counts = df['remote_type'].value_counts().to_dict()
-remote_val = remote_counts.get('Remote', 0)
-onsite_val = remote_counts.get('Onsite', 0)
-hybrid_val = remote_counts.get('Hybrid', 0)
-
-# 3. Pekerjaan yang paling popular
-top_job = data['job_title'].mode()[0]
+try:
+    data = load_data()
 
 # --- Displaying summary boxes ---
 col1, col2, col3 = st.columns(3)
