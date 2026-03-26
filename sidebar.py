9f9f43756e41c482cec4af01f455597adb240d69
atyn104 <s22a0014@siswa.umk.edu.my>
2026-03-26 16:04:31 +0800

    Add sidebar navigation for multiple pages

---
+++ sidebar.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="💼 AI & Data Science Job Market Dashboard "
+)
+
+# 1. Definisi Halaman (Pastikan fail 'Performance Metrics.py', 'Objective 2.py', dan 'Objective 3.py' wujud)
+home = st.Page('home.py', title='Homepage', default=True, icon=":material/home:")
+visualise = st.Page('Performance Metrics.py', title='Objective 1', icon=":material/ads_click:")
+objective2 = st.Page('obj number 2.py', title='Objective 2', icon=":material/ads_click:")
+objective3 = st.Page('obj number 3.py', title='Objective 3', icon=":material/ads_click:")
+
+# 2. Tambahkan semua halaman ke dalam navigasi
+# Semua halaman dalam senarai akan dipaparkan di bawah tajuk 'Menu'
+pg = st.navigation(
+        {
+            "Menu": [home, visualise, objective2, objective3]
+        }
+    )
+
+pg.run()
