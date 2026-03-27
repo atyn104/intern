fc4500584d31708c42430f168ca8eb2eee31f592
atyn104 <s22a0014@siswa.umk.edu.my>
2026-03-27 10:15:17 +0800

    Rename page for Objective 1 in sidebar.py

--- sidebar.py
+++ sidebar.py
@@ -6,7 +6,7 @@ st.set_page_config(
 
 # 1. Definisi Halaman (Pastikan fail 'Performance Metrics.py', 'Objective 2.py', dan 'Objective 3.py' wujud)
 home = st.Page('home.py', title='Homepage', default=True, icon=":material/home:")
-visualise = st.Page('Performance Metrics.py', title='Objective 1', icon=":material/ads_click:")
+objective1 = st.Page('obj number 1.py', title='Objective 1', icon=":material/ads_click:")
 objective2 = st.Page('obj number 2.py', title='Objective 2', icon=":material/ads_click:")
 objective3 = st.Page('obj number 3.py', title='Objective 3', icon=":material/ads_click:")
 
